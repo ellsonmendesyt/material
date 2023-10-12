febee20e618945fdc49e87a7cc942c2f1d478502
ellsonmendesyt <elsonmendesoficial@gmail.comm>
2023-10-12 16:48:54 -0300

    sidenav com propriedades

--- src/app/Material/Material.module.ts
+++ src/app/Material/Material.module.ts
@@ -4,7 +4,7 @@ import {MatSliderModule} from "@angular/material/slider"
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 
@@ -18,6 +18,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatIconModule,
     MatToolbarModule,
     MatButtonModule,
+    MatSidenavModule,
   ],
   declarations: []
 })
