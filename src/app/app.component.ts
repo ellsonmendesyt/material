2175f3d0b3872562f7d215acb66e2e6da0b76aab
ellsonmendesyt <elsonmendesoficial@gmail.comm>
2023-10-12 17:14:30 -0300

    #1 sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import {BreakpointObserver} from "@angular/cdk/layout"
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  
+
 })
 export class AppComponent {
   title = 'curso';
+
+  @ViewChild(MatSidenav) sidenav!:MatSidenav; //!ele pode ser null
+
+  constructor(private bpo:BreakpointObserver){}
+
+  ngAfterContentInit(): void {
+    this.bpo.observe(['(max-width:800px']).subscribe({
+      next:(res)=>{
+        if(res.matches){
+           this.sidenav.close();
+           this.sidenav.mode="over";
+          }else{
+            this.sidenav.open();
+            this.sidenav.mode="side";
+
+        }
+      }
+    })
+  }
 }
